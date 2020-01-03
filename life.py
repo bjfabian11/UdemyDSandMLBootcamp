a0bb71434eb8375ab18f3b49a442a2fd84a52473
Brandon Fabian <brandonfabian.dev@gmail.com>
2020-01-03 14:35:49 -0500

    half way through sectionsection 3 almost done

---
+++ life.py
@@ -0,0 +1,9 @@
+import math
+
+theAnswer = 42
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.')
+    
+def square_root(z):
+    return math.sqrt(z)
